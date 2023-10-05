dc3dd816fcc8b8a4e9baeeb1dc4c1ae932e23c13
JoshFVO <joshmahabir2024@u.northwestern.edu>
2023-10-04 21:28:21 -0500

    HeaderStart

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+
+const Header = () => {
+    return(
+        <div className="HeaderStyle">
+            <div><h2>CatsCrib <img src="src/Components/HeaderImage.png"/></h2></div>
+        </div>)
+
+}
+
+export default Header
