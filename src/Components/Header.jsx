abf0cf670162950341e552e1a16664ad02e7333f
JoshFVO <joshmahabir2024@u.northwestern.edu>
2023-10-04 22:47:35 -0500

    Header Second Changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,10 @@ import "./Header.css"
 const Header = () => {
     return(
         <div className="HeaderStyle">
-            <div><h2>CatsCrib <img src="src/Components/HeaderImage.png"/></h2></div>
-        </div>)
+            <div><h2>CatsCrib <img className="logoImage" src="src/Components/HeaderImage.png"/><img className="profilePhoto" src="src/Components/UserImage.png"/></h2></div>
+            <div><hr></hr></div>
+        </div>
+        )
 
 }
 
