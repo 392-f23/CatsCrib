12862112f3308e9bb988889f69e925e2990d8868
ploenvora <p.voraprukpisut@gmail.com>
2023-10-06 15:42:59 -0500

    Cleaned CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Search from './components/Search';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
         <Header></Header>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-    return(
-        <div className="HeaderStyle">
-            <div><h2>CatsCrib <img className="logoImage" src="src/Components/HeaderImage.png"/><img className="profilePhoto" src="src/Components/UserImage.png"/></h2></div>
-            <div><hr></hr></div>
-        </div>
-        )
+  return (
+    <div className="HeaderStyle">
+      <div>
+        <h2>
+          CatsCrib{" "}
+          <img className="logoImage" src="src/Components/HeaderImage.png" />
+          <img className="profilePhoto" src="src/Components/UserImage.png" />
+        </h2>
+      </div>
+      <div>
+        <hr></hr>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Header
+export default Header;
