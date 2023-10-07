34713c84835add4c9e50e25dea2bc5f5e1f6ca36
ploenvora <p.voraprukpisut@gmail.com>
2023-10-07 01:52:54 -0500

    Added popup

--- src/Components/MoreInfo.jsx
+++ src/Components/MoreInfo.jsx
@@ -7,8 +7,11 @@ const MoreInfo = ({ data, closeHandler }) => {
         <button className="close-btn" onClick={closeHandler}>
           X
         </button>
-        <p className="title">Additional Details</p>
-        <p>{data.roomates}</p>
+        <p className="title">More Info...</p>
+        <p>Subleaser 🧑: {data.user}</p>
+        <p>Roommates 🛏️: {data.roommates}</p>
+        {data.type === 'house' && <p>Housemates 🏠: {data.housemates}</p>}
+        {data.type === 'apartment' && <p>Apartment-mates 🏠: {data.housemates}</p>}
         <p>{data.more_info}</p>
       </div>
     </div>
