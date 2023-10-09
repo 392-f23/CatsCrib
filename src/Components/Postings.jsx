09fc7e567fafe87335734cf44336a7e9a88f817b
ploenvora <p.voraprukpisut@gmail.com>
2023-10-08 20:55:10 -0500

    Styling

--- src/Components/Postings.jsx
+++ src/Components/Postings.jsx
@@ -11,16 +11,16 @@ const Postings = () => {
   const [isFiltering, setIsFiltering] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("sublet");
 
-
   // if data exists, set the postings to the dataset
   // everytime data changes or the page reloads the postings is resetted!
   useEffect(() => {
     if (data) {
-      const filteredPostings = Object.values(data).filter(post => post.category === selectedCategory);
+      const filteredPostings = Object.values(data).filter(
+        (post) => post.category === selectedCategory
+      );
       setPostings(filteredPostings);
     }
-}, [data, selectedCategory]);
-
+  }, [data, selectedCategory]);
 
   // function for filter popup on the side
   const filterHandler = () => {
@@ -30,19 +30,21 @@ const Postings = () => {
   return (
     <div className="postings">
       <div className="category-buttons">
-        <button 
-          className={selectedCategory === "sublet" ? "active" : ""} 
-          onClick={() => setSelectedCategory("sublet")}>
+        <button
+          className={selectedCategory === "sublet" ? "active" : ""}
+          onClick={() => setSelectedCategory("sublet")}
+        >
           Sublet
         </button>
-        <button 
-          className={selectedCategory === "roommate" ? "active" : ""} 
-          onClick={() => setSelectedCategory("roommate")}>
+        <button
+          className={selectedCategory === "roommate" ? "active" : ""}
+          onClick={() => setSelectedCategory("roommate")}
+        >
           Roommate
         </button>
       </div>
       <div className="filter-and-sort">
-        {data && <p className="results-shown">{data.length} RESULTS SHOWN</p>}
+        {data && <p className="results-shown">{postings.length} RESULTS SHOWN</p>}
         {data && <button onClick={filterHandler}>FILTER ▼</button>}
       </div>
       {postings.map((data, index) => (
