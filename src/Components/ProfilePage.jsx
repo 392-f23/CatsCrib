042285dfc0e5be32a9ceeabb2316422187893c44
JoshFVO <joshmahabir2024@u.northwestern.edu>
2023-10-08 21:52:55 -0500

    Profile Page Updated

--- src/Components/ProfilePage.jsx
+++ src/Components/ProfilePage.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import "./ProfilePage.css"
+import "./ProfilePage.css";
+import Footer from "./Footer";
 
 
 const ProfilePage = () => {
     return (
         <div className="body">
-            <img className="ProfilePagePhoto" src="public/icons/ProfilePagePhoto.jpeg"></img>
+            <img className="ProfilePagePhoto" src="/icons/ProfilePagePhoto.jpeg"></img>
             <h2>Joshua Mahabir</h2>
             <h3>@JoshFVO</h3>
             <br></br>
             <div className="ProfileInfo">
-            <p><img className="EmailLogo" src="public/icons/EmailLogo.png"></img>JoshuaMahabir2024@u.northwestern.edu</p>
-            <p><img className="EmailLogo" src="public/icons/PhoneLogo.png"/>718-749-6412</p>
-            <p><img className="EmailLogo" src="public/icons/PronounLogo.png"/>He/Him</p>
+            <p><img className="EmailLogo" src="/icons/EmailLogo.png"></img>JoshuaMahabir2024@u.northwestern.edu</p>
+            <p><img className="EmailLogo" src="/icons/PhoneLogo.png"/>718-749-6412</p>
+            <p><img className="EmailLogo" src="/icons/PronounLogo.png"/>He/Him</p>
             </div>
+            <Footer></Footer>
         </div>
     )
 }
